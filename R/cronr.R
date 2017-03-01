3c2487c6fa01a60f840bf6f5a3f306a5f76c5534
henfiber <henfiber@gmail.com>
2017-03-01 16:25:55 +0200

    better formatting in summary doc

--- R/cronr.R
+++ R/cronr.R
@@ -1,10 +1,12 @@
-#' cronr: A Cron-like second-precision Scheduler for R
+#' cronr
+#'
+#' A Cron-like second-precision Scheduler for R
 #'
 #' This is a scheduling package for running one-off or recurring tasks in second-precision schedules.
 #' It may be useful either in maintenance tasks, or in batch processing pipelines where new data are made available periodically.
 #' For example, you may need to retrieve new records written in a database every 30 seconds and process them in batches.
 #'
-#' @section crond functions:
+#' @section cronr functions:
 #' crond
 #'
 #' @docType package
