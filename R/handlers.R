bae67207e07b6336a17f1912dba354d5ab932e16
henfiber <henfiber@gmail.com>
2017-03-01 18:40:49 +0200

    Added documentation for the dummy handler

--- R/handlers.R
+++ R/handlers.R
@@ -1,5 +1,12 @@
 # Some sample handlers
 
+#' dummy_task_handler
+#'
+#' A dummy task handler that just prints an informative message. 
+#' Arguments do not need to be supplied, they will passed by crond
+#'
+#' 
+#' @export
 dummy_task_handler <- function(task_id, timestamp, tasks_info) {
     # Do something with this "task_id" using the information in "tasks_info"
     message("Running ", task_id, " at ", unclass(Sys.time()), " - scheduled: ",
